e5c0aa4993faef84365ae28e09057a2846f6d7f9
Laurette <siler.laurette@gmail.com>
2018-03-25 17:58:40 -0400

    Initial header css

--- lib/header/header.jsx
+++ lib/header/header.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
 
 export default class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = { selected: '1' }
+    this.currentChoice = this.currentChoice.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  currentChoice(idx) {
+    if (idx === this.state.selected) {
+      return 'selected';
+    } else {
+      return '';
+    }
+  }
+
+  handleToggle(idx) {
+    return () => {
+      this.setState({ selected: idx })
+    };
+  }
 
   render() {
     return (
       <div className='header'>
-        <div>About Me</div>
-        <div>Projects</div>
-        <div>Contact</div>
+        <div
+          id='about-me'
+          className={`about-header ${this.currentChoice('1')}`}
+          onClick={this.handleToggle('1')}>About Me</div>
+        <div
+          id='projects'
+          className={`about-header ${this.currentChoice('2')}`}
+          onClick={this.handleToggle('2')}>Projects</div>
+        <div
+          id='contact'
+          className={`about-header ${this.currentChoice('3')}`}
+          onClick={this.handleToggle('3')}>Contact</div>
       </div>
     );
   }
--- lib/root.jsx
+++ lib/root.jsx
@@ -4,7 +4,7 @@ import App from './app';
 const Root = () => {
   return (
     <App />
-  )
+  );
 };
 
 export default Root;

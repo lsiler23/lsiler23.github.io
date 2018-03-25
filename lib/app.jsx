19956d07fc83764fe7ad646f071a55c9414b4e82
Laurette <siler.laurette@gmail.com>
2018-03-25 17:16:32 -0400

    Create header component

---
+++ lib/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from './header/header';
+
+const App = () => (
+  <Header />
+);
+
+export default App;
---
+++ lib/header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+
+  render() {
+    return (
+      <div className='header'>
+        <div>About Me</div>
+        <div>Projects</div>
+        <div>Contact</div>
+      </div>
+    );
+  }
+}
